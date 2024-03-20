750a347adaa55e095fc5441234ca3863680c529d
kkushi7 <100981849+kkushi7@users.noreply.github.com>
2024-03-20 00:31:06 -0700

    Update common.py

--- common.py
+++ common.py
@@ -107,8 +107,7 @@ def import_data(fpath):
 
 def insert_student(UCINetID, email, first, middle, last):
     try:
-        cursor.execute('''INSERT INTO students(UCINetID, email, first, middle, last)
-                      VALUES(?, ?, ?, ?, ?)''',(UCINetID, email, first, middle, last))
+        cursor.execute("INSERT INTO students(UCINetID, email, first, middle, last)VALUES(?, ?, ?, ?, ?)",(UCINetID, email, first, middle, last))
         db_connection.commit()
         print("Success")
     except mysql.Error as e:
