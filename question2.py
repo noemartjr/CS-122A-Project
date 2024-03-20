905a754adc66c52f1f42c690421e3949e166a4db
kkushi7 <100981849+kkushi7@users.noreply.github.com>
2024-03-20 00:27:23 -0700

    Update question2.py

--- question2.py
+++ question2.py
@@ -1,11 +1,8 @@
-import mysql.connector as m
-import sys
-
-a = m.connect(host = "localhost", user = "root", password = "iL0v3BTS!")
-if a.is_connected():
-    print("success")
-b = a.cursor()
-b.execute("DROP TABLE IF EXISTS students;")
-b.execute("CREATE TABLE students(UCINetID STR(20) PRIMARY KEY NOT NULL, Email STR(1024), FirstName STR(50), MiddleName VARCHAR(50), LastName VARCHAR(50), FOREIGN KEY (UCINetID) REFERENCES Users(UCINetID) ON DELETE CASCADE, FOREIGN KEY (Email) REFERENCES Emails(Email) ON DELETE CASCADE);")
-b.execute("INSERT INTO students(UCINetID, Email, FirstName, MiddleName, LastName) VALUES (%s, %s);")
-a.commit()
+def insert_student(UCINetID, email, first, middle, last):
+    try:
+        cursor.execute('''INSERT INTO students(UCINetID, email, first, middle, last)
+                      VALUES(?, ?, ?, ?, ?)''',(UCINetID, email, first, middle, last))
+        db_connection.commit()
+        print("Success")
+    except mysql.Error as e:
+        print("Fail")
